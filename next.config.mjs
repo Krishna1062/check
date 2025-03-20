9d969b65a7114d787d4d65fc865427fb96502586
Krishna <krishna161062@gmail.com>
2025-03-20 15:19:42 +0530

    the commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    exportTrailingSlash: true, // Ensures clean URLs (e.g., /about/ instead of /about.html)
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false, // Fix for browser compatibility (e.g., "fs" module used in Node.js)
+        };
+      }
+      return config;
+    },
+  };
+  
 export default nextConfig;
